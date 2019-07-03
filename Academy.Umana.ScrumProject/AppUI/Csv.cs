b65ac6bc6e6503d009123d2923a966e8d15cb857
prova <prova@hotmail.it>
2019-07-03 17:57:43 +0200

    modifiche visualizzzione viste

--- Academy.Umana.ScrumProject/AppUI/Csv.cs
+++ Academy.Umana.ScrumProject/AppUI/Csv.cs
@@ -1,38 +1,60 @@
 ﻿using AppLogic.Entities;
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AppUI
 {
-   public class Csv
+    public class Csv
     {
-         public Classe[] LoadGraph(string path)
-           {
-               Classe[] classi = new Classe[200];
-               string[] content = File.ReadAllLines(path).Skip(1).ToArray();
-               int index = 0;
+        public Classe[] LoadGraph(string path)
+        {
+            Classe[] classi = new Classe[200];
+            string[] content = File.ReadAllLines(path).Skip(1).ToArray();
+            int index = 0;
 
-               foreach (string line in content)
-               {
-                   string[] values = line.Split(new char[] { ',' });
+            foreach (string line in content)
+            {
+                string[] values = line.Split(new char[] { ',' });
 
-                   classi[index] = new Classe()
-                   {
-                       ID_Classe =  int.Parse(values[0]),
-                       Sezione =  (values[1]),
-                       Anno = (values[2])                  
-                   };
-                   index++;
+                classi[index] = new Classe()
+                {
+                    ID_Classe = int.Parse(values[0]),
+                    Sezione = (values[1]),
+                    Anno = (values[2])
+                };
+                index++;
 
-               }
-               return classi; 
+            }
+            return classi;
 
-           }
+        }
 
+
+        public Alunno[] LoadGraphAlunno(string path)
+        {
+            Alunno[] alunni = new Alunno[200];
+            string[] content = File.ReadAllLines(path).Skip(1).ToArray();
+            int index = 0;
+
+            foreach (string line in content)
+            {
+                string[] values = line.Split(new char[] { ',' });
+
+                alunni[index] = new Alunno()
+                {
+                    ID_Alunno = int.Parse(values[0]),
+                    Nome = (values[1]),
+                    Cognome = (values[2]),
+                    FK_Classe = int.Parse(values[3])
+                };
+                index++;
+            }
+            return alunni;
+        }
 
     }
+
+
 }
+
+

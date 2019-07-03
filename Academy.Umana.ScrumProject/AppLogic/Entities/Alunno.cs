1dee5c10ba95d27b740d8b9c6f21556acf5f7bc2
Tanjin <tanjin@gmail.com>
2019-07-03 16:06:34 +0200

    aggiunto file contentente la logica

---
+++ Academy.Umana.ScrumProject/AppLogic/Entities/Alunno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppLogic.Entities
+{
+    class Alunno
+    {
+        public int ID_Alunno { get; set; }
+        public string Nome { get; set; }
+        public string Cognome{ get; set; }
+        public int FK_Classe { get; set; }
+    }
+}
